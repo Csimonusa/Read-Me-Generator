7f82fb412070c677b786ffc9a3b5709f942713fb
mona <csimonusa@outlook.com>
2022-01-20 15:43:54 -0500

    added questions to index.js

---
+++ index.js
@@ -0,0 +1,59 @@
+const inquirer = require("inquirer")
+const fs = require("fs")
+
+inquirer
+  .prompt([
+      {
+        type: 'input',
+        message: 'What is your Github username?',
+        name: 'githubUser'
+      },
+      {
+        type: 'input',
+        message: "What is your project's name?",
+        name: 'projectName'
+      },
+      {
+        type: 'input',
+        message: "What is your project's description?",
+        name: 'description'
+      },
+      {
+        type: 'confirm',
+        message: 'Include table of contents?',
+        name: 'confirmToc',
+        default: true
+      },
+      {
+        type: 'input',
+        message: 'Enter the steps for installing your project.',
+        name: 'installation'
+      },
+      {
+        type: 'input',
+        message: 'How can your project be used?',
+        name: 'usage'
+      },
+      {
+        type: 'list',
+        message: 'Select a license for your project',
+        name: 'license',
+        choices: ["MIT", "ISC"],
+        default: ["MIT"],
+      },
+      {
+        type: 'input',
+        message: 'How can others contribute to your project?',
+        name: 'contributing'
+      },
+      {
+        type: 'input',
+        message: 'How can others test your project?',
+        name: 'test'
+      },
+      {
+        type: 'input',
+        message: 'Enter an email so others can contact you regarding this project.',
+        name: 'questions'
+      },
+  ])
